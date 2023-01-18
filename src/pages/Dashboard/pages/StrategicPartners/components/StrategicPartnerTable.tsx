f1c7eea5a98a3793faaaa437b173d2dd9e1f6be1
olamide jubril <olamidejubril68@gmail.com>
2023-01-18 12:11:51 +0100

    Adjust the loading of all the admin sections

--- src/pages/Dashboard/pages/StrategicPartners/components/StrategicPartnerTable.tsx
+++ src/pages/Dashboard/pages/StrategicPartners/components/StrategicPartnerTable.tsx
@@ -73,7 +73,7 @@ const StrategicPartnerTable = ({
 
         <Table.Body className="divide-y">
           {loading && (
-            <div className="container flex items-center justify-center my-8 ml-[280px]">
+            <div className="container flex items-center justify-center my-8 ml-[200px]">
               <Spinner
                 color="success"
                 aria-label="spinner"
