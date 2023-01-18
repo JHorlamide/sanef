b654f5c8060ae6240f73f3dcde70be526695358a
olamide jubril <olamidejubril68@gmail.com>
2023-01-18 12:17:43 +0100

    Centered the spinner of the admin section

--- src/pages/Dashboard/pages/Regulators/components/RegulatorsListTable.tsx
+++ src/pages/Dashboard/pages/Regulators/components/RegulatorsListTable.tsx
@@ -73,7 +73,7 @@ const RegulatorsListTable = ({
 
         <Table.Body className="divide-y">
           {loading && (
-            <div className="container flex items-center justify-center my-8 ml-[150px]">
+            <div className="container flex items-center justify-center my-8 ml-[180px]">
               <Spinner
                 color="success"
                 aria-label="spinner"
